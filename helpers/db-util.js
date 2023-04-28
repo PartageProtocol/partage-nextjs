0df2b5de850e5c3a797875b45781c7e9f13cc6c8
Julien Carbonnell <julien.carbonnell@gmail.com>
2023-04-28 13:18:13 -0700

    Add files via upload

---
+++ helpers/db-util.js
@@ -0,0 +1,30 @@
+import { MongoClient } from "mongodb";
+
+export async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://juliencarbonnell:ULmK83VeJOKBSJGJ@cluster0.ygkjxo9.mongodb.net/newsletter?retryWrites=true&w=majority"
+  );
+
+  return client;
+}
+
+export async function insertDocument(client, collection, document) {
+  const db = client.db();
+
+  const result = await db.collection(collection).insertOne(document);
+
+  return result;
+}
+
+export async function getAllDocuments(client, collection, sort) {
+  // connect to db and fetch all comments
+  const db = client.db();
+
+  const documents = await db
+  .collection(collection)
+  .find()
+  .sort(sort)
+  .toArray();
+
+  return documents;
+}
