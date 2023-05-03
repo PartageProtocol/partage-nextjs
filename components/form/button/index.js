e19027671e3e46fc6ac0dfacaa59c3d2804c1993
leonace924 <meobius123@gmail.com>
2023-05-03 05:24:17 -0400

    feat: add button COMP

---
+++ components/form/button/index.js
@@ -0,0 +1,52 @@
+import Link from 'next/link'
+import clsx from 'clsx'
+import styles from './button.module.css'
+
+const Button = ({
+  variant = 'primary',
+  href,
+  icon,
+  label,
+  onClick,
+  className,
+  ...props
+}) => {
+  if (href) {
+    return (
+      <Link
+        href={href}
+        className={clsx(
+          styles.btn,
+          {
+            [styles['btn--primary']]: variant === 'primary',
+            [styles['btn--secondary']]: variant === 'secondary',
+          },
+          className
+        )}
+      >
+        {icon && <span className={styles.btn__icon}>{icon}</span>}
+        {label && <span>{label}</span>}
+      </Link>
+    )
+  }
+
+  return (
+    <button
+      className={clsx(
+        styles.btn,
+        {
+          [styles['btn--primary']]: variant === 'primary',
+          [styles['btn--secondary']]: variant === 'secondary',
+        },
+        className
+      )}
+      onClick={onClick}
+      {...props}
+    >
+      {icon && <span className={styles.btn__icon}>{icon}</span>}
+      {label && <span>{label}</span>}
+    </button>
+  )
+}
+
+export default Button
